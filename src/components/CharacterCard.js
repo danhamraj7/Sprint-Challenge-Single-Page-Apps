3b679271bef1ec4dc243dd7157f14077561d5cb3
danhamraj <danhamraj@gmail.com>
2019-11-15 12:06:59 -0500

    Created characterCard.js, characterList.js started LocationCard.js created path for characterList on App.js

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,37 @@
 import React from "react";
+import styled from "styled-components";
 
-export default function CharacterCard() {
-  return <span>todo: character</span>;
+const StyledCharacter = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 40%;
+  margin: 1em;
+  padding: 1em;
+`;
+
+const StyledInnerCharacter = styled.div`
+  border: 0.5em solid purple;
+  padding: 1em;
+`;
+
+export default function CharacterCard(props) {
+  const { image, name, species, status, location } = props;
+
+  return (
+    <StyledCharacter>
+      <StyledInnerCharacter>
+        <img src={image} alt="character" />
+      </StyledInnerCharacter>
+
+      <StyledInnerCharacter>
+        <h4>{name}</h4>
+
+        <p>{species}</p>
+
+        <p>{status}</p>
+
+        <p>{location}</p>
+      </StyledInnerCharacter>
+    </StyledCharacter>
+  );
 }
